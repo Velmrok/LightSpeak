eba559c100aaf1376e847ccc30e54cf8cf3a6b99
Maciej Spławiński <splawis3@gmail.com>
2026-07-15 22:48:37 +0200

    add auth endpoints

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -1,5 +1,11 @@
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using Duende.AccessTokenManagement.OpenIdConnect;
+using Gateway.src;
 using Gateway.src.Extensions;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +15,9 @@ builder.Configuration
 builder.Services.AddStackExchangeRedisCache(o =>
     o.Configuration = builder.Configuration.GetConnectionString("Redis"));
 
-
+builder.Services.AddOpenIdConnectAccessTokenManagement();
+builder.Services.AddAuthorization();
+builder.Services.AddReverseProxy(builder.Configuration);
 builder.Services.AddServices(builder.Configuration);
 builder.Services.AddAuth(builder.Configuration);
     
@@ -17,10 +25,22 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("users/me", (ClaimsPrincipal claim) =>
+app.MapGet("/users/me", (ClaimsPrincipal user) => new UserDto(
+    Id:    user.FindFirstValue(JwtRegisteredClaimNames.Sub)!,
+    Name:  user.Identity!.Name,
+    Email: user.FindFirstValue(JwtRegisteredClaimNames.Email),
+    Roles: user.FindAll("roles").Select(c => c.Value).ToArray()
+)).RequireAuthorization();
+
+app.MapPost("/logout", async ctx =>
 {
-    return claim.Claims.ToDictionary(c => c.Type, c => c.Value);
+    await ctx.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    await ctx.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme, new AuthenticationProperties { RedirectUri = "/" });
+    
 }).RequireAuthorization();
+
+app.MapGet("/login", async (HttpContext context) =>
+    Results.Challenge(new AuthenticationProperties { RedirectUri = "/" }) );
 
 app.MapReverseProxy();
 app.UseAuthentication();

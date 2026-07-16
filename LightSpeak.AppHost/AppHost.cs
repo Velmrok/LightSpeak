59431842577dfaf37c3cf571bce39299ce51a07b
Maciej Spławiński <splawis3@gmail.com>
2026-07-16 16:19:03 +0200

    add missing parameters

--- LightSpeak.AppHost/AppHost.cs
+++ LightSpeak.AppHost/AppHost.cs
@@ -2,26 +2,33 @@ using Aspire.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var kcAdminUser = builder.AddParameter("kc-admin-user", secret: true);
-var kcAdminPassword = builder.AddParameter("kc-admin-password", secret: true);
-var kcGatewaySecret = builder.AddParameter("kc-gateway-secret", secret: true);
+var kcAdminUser = builder.AddParameter("kc-admin-user");
+var kcAdminPassword = builder.AddParameter("kc-admin-password"/*, secret: true*/);
+var kcGatewaySecret = builder.AddParameter("kc-gateway-secret"/*, secret: true*/);
+var kcAdminSecret = builder.AddParameter("kc-admin-client-secret"/*, secret: true*/);
 var appBaseUrl = builder.AddParameter("app-base-url");
 
 
 var redis = builder.AddRedis("redis").WithLifetime(ContainerLifetime.Persistent);
-var keycloak = builder.AddKeycloak("keycloak",8080,kcAdminUser,kcAdminPassword).WithLifetime(ContainerLifetime.Persistent);
+var keycloak = builder.AddKeycloak("keycloak",8080,kcAdminUser,kcAdminPassword)
+    .WithHttpEndpoint(name: "cli", targetPort: 8080) 
+    .WithImageTag("26.1.0")
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithEnvironment("KC_HTTP_ENABLED", "true");
 
 var gateway = builder.AddProject<Projects.Gateway>("gateway")
+    .WithHttpEndpoint(port:5067)
     .WithReference(redis)
     .WithReference(keycloak)
-    .WithEnvironment("OpenIDConnectSettings__Authority",$"{keycloak.GetEndpoint("http")}/realms/lightspeak")
+    .WithEnvironment("OpenIDConnectSettings__Authority",$"{keycloak.GetEndpoint("cli")}/realms/lightspeak")
     .WithEnvironment("OpenIDConnectSettings__ClientSecret", kcGatewaySecret)
+    .WithEnvironment("OpenIDConnectSettings__ClientId", "light-speak-gateway")
     .WaitFor(keycloak)
     .WaitFor(redis);
 
-var kcConfig = builder.AddContainer("keycloak-config", "adorsys/keycloak-config-cli", "6.4.0-26.6.0")
+var kcConfig = builder.AddContainer("keycloak-config", "adorsys/keycloak-config-cli", "6.5.1-26.1.0")
     .WithBindMount("../keycloak", "/config", isReadOnly: true)
-    .WithEnvironment("KEYCLOAK_URL", keycloak.GetEndpoint("http"))
+    .WithEnvironment("KEYCLOAK_URL", keycloak.GetEndpoint("cli"))
     .WithEnvironment("KEYCLOAK_USER", kcAdminUser)
     .WithEnvironment("KEYCLOAK_PASSWORD", kcAdminPassword)
     .WithEnvironment("KEYCLOAK_AVAILABILITYCHECK_ENABLED", "true")
@@ -30,6 +37,8 @@ var kcConfig = builder.AddContainer("keycloak-config", "adorsys/keycloak-config-
     .WithEnvironment("IMPORT_VARSUBSTITUTION_ENABLED", "true")
     .WithEnvironment("APP_BASE_URL", appBaseUrl)
     .WithEnvironment("KC_GATEWAY_SECRET", kcGatewaySecret)
+    .WithEnvironment("KC_ADMIN_CLIENT_SECRET", kcAdminSecret)
     .WaitFor(keycloak);
+gateway.WaitForCompletion(kcConfig);
 
 builder.Build().Run();

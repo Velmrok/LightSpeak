10437040d9c6d7c39c7304ffbf858f5fe6a8c021
Maciej Spławiński <splawis3@gmail.com>
2026-07-20 19:53:58 +0200

    Change configuration of the app

--- LightSpeak.AppHost/AppHost.cs
+++ LightSpeak.AppHost/AppHost.cs
@@ -17,16 +17,17 @@ var appBaseUrl = builder.AddParameter("app-base-url");
 var redis = builder.AddRedis("redis").WithLifetime(lifetime);
 
 var keycloak = builder.AddKeycloak("keycloak",null,kcAdminUser,kcAdminPassword)
-    .WithHttpEndpoint(name: "cli", targetPort: 8080) 
+    .WithHttpEndpoint(name: "keycloak", targetPort: 8080) 
     .WithImageTag("26.1.0")
     .WithLifetime(lifetime)
     .WithEnvironment("KC_HTTP_ENABLED", "true");
 
 var gateway = builder.AddProject<Projects.Gateway>("gateway")
-    .WithHttpEndpoint(port:5067)
     .WithReference(redis)
     .WithReference(keycloak)
-    .WithEnvironment("OpenIDConnectSettings__Authority",$"{keycloak.GetEndpoint("cli")}/realms/lightspeak")
+    .WithEnvironment("ASPNETCORE_URLS", appBaseUrl)
+    .WithEnvironment("AppBaseUrl", appBaseUrl)
+    .WithEnvironment("OpenIDConnectSettings__Authority",$"{keycloak.GetEndpoint("keycloak")}/realms/lightspeak")
     .WithEnvironment("OpenIDConnectSettings__ClientSecret", kcGatewaySecret)
     .WithEnvironment("OpenIDConnectSettings__ClientId", "light-speak-gateway")
     .WaitFor(keycloak)
@@ -34,17 +35,25 @@ var gateway = builder.AddProject<Projects.Gateway>("gateway")
 
 var kcConfig = builder.AddContainer("keycloak-config", "adorsys/keycloak-config-cli", "6.5.1-26.1.0")
     .WithBindMount("../keycloak", "/config", isReadOnly: true)
-    .WithEnvironment("KEYCLOAK_URL", keycloak.GetEndpoint("cli"))
+    .WithEnvironment("KEYCLOAK_URL", keycloak.GetEndpoint("keycloak"))
     .WithEnvironment("KEYCLOAK_USER", kcAdminUser)
     .WithEnvironment("KEYCLOAK_PASSWORD", kcAdminPassword)
     .WithEnvironment("KEYCLOAK_AVAILABILITYCHECK_ENABLED", "true")
     .WithEnvironment("KEYCLOAK_AVAILABILITYCHECK_TIMEOUT", "120s")
     .WithEnvironment("IMPORT_FILES_LOCATIONS", "/config/*.yaml")
     .WithEnvironment("IMPORT_VARSUBSTITUTION_ENABLED", "true")
-    .WithEnvironment("APP_BASE_URL", appBaseUrl)
     .WithEnvironment("KC_GATEWAY_SECRET", kcGatewaySecret)
     .WithEnvironment("KC_ADMIN_CLIENT_SECRET", kcAdminSecret)
+    .WithEnvironment("KC_TESTUSER_ENABLED", isTesting ? "true" : "false")
     .WaitFor(keycloak);
+if (isTesting)
+{
+    kcConfig.WithEnvironment("APP_BASE_URL", "*");
+}
+else
+{
+    kcConfig.WithEnvironment("APP_BASE_URL", appBaseUrl);
+}
 gateway.WaitForCompletion(kcConfig);
 
 builder.Build().Run();

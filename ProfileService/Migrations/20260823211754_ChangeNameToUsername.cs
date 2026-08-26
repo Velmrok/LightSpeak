0a5a6ac0bea23817a6284de2f011bdf41b2ce118
Maciej Spławiński <splawis3@gmail.com>
2026-08-26 14:14:43 +0200

    Change name to username migration

---
+++ ProfileService/Migrations/20260823211754_ChangeNameToUsername.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProfileService.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeNameToUsername : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Profiles",
+                newName: "Username");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Profiles",
+                newName: "Name");
+        }
+    }
+}

5e6304f17e321d6d76a537c407cc9c32d4b81012
Maciej Spławiński <splawis3@gmail.com>
2026-08-27 17:38:28 +0200

    Add CreateProfile test

--- LightSpeak.Tests/src/ProfileServiceTest.cs
+++ LightSpeak.Tests/src/ProfileServiceTest.cs
@@ -1,28 +1,31 @@
 using Aspire.Hosting;
 using Common.Dto;
 using JasperFx.Events.Documents;
+using LightSpeak.AppHost.src.Constants;
 using ProfileService.src.database;
+using ProfileService.src.services;
 
 namespace LightSpeak.Tests.src;
 [Collection("Aspire")]
 public class ProfileServiceTest : TestBase
 {
-    
+   
    
     public ProfileServiceTest(AppFixture fixture) : base(fixture)
     {
+       
     }
 
     private async Task<AppDbContext> CreateDbContext(CancellationToken ct)
     {
-        return await Fixture.CreateDbContextAsync<AppDbContext>("profile-database", ct);
+        return await Fixture.CreateDbContextAsync<AppDbContext>(ResourcesNames.ProfileDatabase, ct);
     }
 
     [Fact]
     public async Task CreatesProfile_Correctly_OnUserRegistrationEvent()
     {
-        var ct = await WaitForResourceRunningAsync("gateway");
-      
+        var ct = CancellationToken.None;
+        await WaitForResourceRunningAsync(ResourcesNames.ProfileService, ct);
 
         var id = Guid.NewGuid().ToString();
         var email = id + "@test.com";
@@ -37,8 +40,36 @@ public class ProfileServiceTest : TestBase
                 Assert.NotNull(profile);
                 Assert.Equal(username, profile.Username);
                 Assert.Equal(email, profile.Email);
-            }, TimeSpan.FromSeconds(60) , ct);
+            }, TimeSpan.FromSeconds(15) , ct);
 
+    }
+    [Fact]
+    public async Task CreateProfile_CreatesRecordInDatabase()
+    {
+        var ct = CancellationToken.None;
+        await WaitForResourceRunningAsync(ResourcesNames.ProfileService, ct);
+        await WaitForResourceRunningAsync(ResourcesNames.ProfileDatabase, ct);
+        var id = Guid.NewGuid().ToString();
+        var email = id + "@test.com";
+        var username = id + "user";
+
+        var profile = new Profile
+        {
+            Id = id,
+            Username = username,
+            Email = email,
+            CreatedAt = DateTime.UtcNow
+        };
+        
+        var profileService = new ProfileApplicationService(await CreateDbContext(ct));
+        var result = await profileService.CreateProfileAsync(profile, ct);
+        Assert.True(result.IsSuccess);
+
+        var dbContext = await CreateDbContext(ct);
+        var createdProfile = dbContext.Profiles.FirstOrDefault(p => p.Id == id);
+        Assert.NotNull(createdProfile);
+        Assert.Equal(username, createdProfile.Username);
+        Assert.Equal(email, createdProfile.Email);
     }
     
 }

327e9cbf04f6f4116cec319e6320752429c68c92
Maciej Spławiński <splawis3@gmail.com>
2026-08-30 22:56:32 +0200

    Add two ProfileService tests

--- LightSpeak.Tests/src/ProfileServiceTest.cs
+++ LightSpeak.Tests/src/ProfileServiceTest.cs
@@ -44,6 +44,29 @@ public class ProfileServiceTest : TestBase
 
     }
     [Fact]
+    public async Task CreatesProfile_Correctly_WithDoubleEventSent()
+    {
+        var ct = CancellationToken.None;
+        await WaitForResourceRunningAsync(ResourcesNames.ProfileService, ct);
+
+        var id = Guid.NewGuid().ToString();
+        var email = id + "@test.com";
+        var username = id + "user";
+        await RabbitMqTestHelper.PublishRegisterEventAsync(Fixture.RabbitMqConnection, id, username, email,ct);
+        await RabbitMqTestHelper.PublishRegisterEventAsync(Fixture.RabbitMqConnection, id, username, email,ct);
+
+        await Eventually.Assert(
+            async () =>
+            {
+                var dbContext = await CreateDbContext(ct);
+                var profile = dbContext.Profiles.FirstOrDefault(p => p.Id == id);
+                Assert.NotNull(profile);
+                Assert.Equal(username, profile.Username);
+                Assert.Equal(email, profile.Email);
+            }, TimeSpan.FromSeconds(15) , ct);
+
+    }
+    [Fact]
     public async Task CreateProfile_CreatesRecordInDatabase()
     {
         var ct = CancellationToken.None;
@@ -70,6 +93,31 @@ public class ProfileServiceTest : TestBase
         Assert.NotNull(createdProfile);
         Assert.Equal(username, createdProfile.Username);
         Assert.Equal(email, createdProfile.Email);
+    }
+    [Fact]
+    public async Task CreateProfile_ReturnsError_WhenProfileAlreadyExists()
+    {
+        var ct = CancellationToken.None;
+        await WaitForResourceRunningAsync(ResourcesNames.ProfileService, ct);
+        await WaitForResourceRunningAsync(ResourcesNames.ProfileDatabase, ct);
+        var id = Guid.NewGuid().ToString();
+        var email = id + "@test.com";
+        var username = id + "user";
+
+        var profile = new Profile
+        {
+            Id = id,
+            Username = username,
+            Email = email,
+            CreatedAt = DateTime.UtcNow
+        };
+        
+        var profileService = new ProfileApplicationService(await CreateDbContext(ct));
+        var result1 = await profileService.CreateProfileAsync(profile, ct);
+        Assert.True(result1.IsSuccess);
+
+        var result2 = await profileService.CreateProfileAsync(profile, ct);
+        Assert.False(result2.IsSuccess);
     }
     
 }

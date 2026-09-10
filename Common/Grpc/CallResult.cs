b4617b6ee1367439fde0de79b28a8773ea0e5e42
Maciej Spławiński <splawis3@gmail.com>
2026-09-10 15:01:12 +0200

    Separate callresult from calloutcome

---
+++ Common/Grpc/CallResult.cs
@@ -0,0 +1,10 @@
+
+namespace Common.Grpc;
+
+public record CallResult<T>(T? Data, AppError? Error);
+
+public static class CallResultExtensions
+{
+    public static CallOutcome<T> AsOutcome<T>(this CallResult<T> result, string section,bool required) =>
+        new(section, required, result.Data, result.Error);
+}
--- Common/Grpc/GrpcCallHandler.cs
+++ Common/Grpc/GrpcCallHandler.cs
@@ -1,4 +1,5 @@
 using Common.Dto;
+using Common.Mappers;
 using Grpc.Core;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -7,29 +8,22 @@ namespace Common.Grpc;
 
 public class GrpcCallHandler
 {
-    public async Task<CallOutcome<T>> SafeCall<T>(
-        string section,
-        bool required,
-        Func<DateTime,Task<T>> call)
+    public async Task<CallResult<T>> SafeCall<T>( Func<DateTime, CancellationToken, Task<T>> call, CancellationToken cancellationToken)
     {
         try
         {
             var deadline = DateTime.UtcNow.AddSeconds(10);
-            var result = await call(deadline);
+            var result = await call(deadline, cancellationToken);
 
-            return new CallOutcome<T>(
-                section,
-                required,
+            return new CallResult<T>(
                 result,
                 null);
         }
         catch (RpcException ex)
         {
-            var appError = new AppError(ex);
+            var appError = ex.ToAppError();
 
-            return new CallOutcome<T>(
-                section,
-                required,
+            return new CallResult<T>(
                 default,
                 appError);
         }
